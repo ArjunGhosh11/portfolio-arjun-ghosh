17660811741f79b6b040cc4059675aae161e5b04
ArjunGhosh11 <ghosh.rj11@gmail.com>
2022-06-09 20:16:21 +0600

    navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,19 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#32a85e",
+          secondary: "#13cf19",
+          accent: "#f79d16",
+          neutral: "#f71616",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
 }
